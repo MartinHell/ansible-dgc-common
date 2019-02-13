42ccb4bdaed21ca8bdffd6da804002a672776bc9
boolman <boolman@gmail.com>
2019-02-13 15:21:56 +0100

    added systemd

---
+++ files/zabbix_agent/systemd_services.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+import argparse
+import os
+import subprocess
+import re
+import json
+
+def Discovery(*kv):
+    d = {"data":[]}
+
+    if len(kv) > 0:
+      white = kv[0]
+    else:
+      white = ['sshd', 'ntp', 'syslog', 'zabbix', 'docker']
+
+    cmd = [ 'systemctl', 'list-unit-files', '--type', 'service', '--state', 'enabled,generated']
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    output, err =  proc.communicate()
+
+    services = re.findall(".*.service", output)
+    whitelist = re.compile('|'.join([re.escape(word) for word in white]))
+    filtered_list = [word for word in services if whitelist.search(word)]
+
+    for service in filtered_list:
+        zbx_item = { "{#SERVICE}": service }
+        d["data"].append(zbx_item)
+
+    return json.dumps(d, indent=4, sort_keys=True)
+
+
+def CheckService(service):
+    cmd = [ 'systemctl', 'status', '--quiet', service ]
+    try:
+        output = subprocess.check_output(cmd)
+        return 1
+    except subprocess.CalledProcessError as e:
+        return 0
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Systemd monitoring')
+    parser.add_argument(
+        '-d',
+        '--discovery',
+        dest='discovery',
+        action='store_true',
+        default=False,
+        help="Discovery")
+    parser.add_argument(
+        '-s',
+        '--service',
+        dest='service',
+        default=None,
+        help="Service to be checked")
+
+    args = parser.parse_args()
+
+    if args.discovery:
+        print(Discovery())
+        return
+    if args.service:
+        print(CheckService(args.service))
+
+
+
+if __name__ == '__main__':
+    main()
